a00801075d35395eda6809d37fe41ed7d24d5952
rahatservice <mehemmedjavziyev@gmail.com>
2026-05-03 01:07:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,7 +9,7 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 
 KANAL_ID = 1499363884593840149
 
-user_counters = {}  # her kullanıcı için sayaç
+user_counters = {}
 
 @bot.event
 async def on_ready():
@@ -18,36 +19,29 @@ async def on_ready():
 async def ant(ctx):
     user_id = ctx.author.id
 
-    # yanlış kanal kontrolü
     if ctx.channel.id != KANAL_ID:
-        kanal = bot.get_channel(KANAL_ID)
+        kanal = await bot.fetch_channel(KANAL_ID)
 
         embed = discord.Embed(
             title="❌ Yanlış Kanal",
-            description=f"Bu komutu burada kullanamazsın.\nDoğru kanal: {kanal.mention}",
+            description=f"Burada olmaz.\nDoğru kanal: {kanal.mention}",
             color=discord.Color.red()
         )
 
         await ctx.send(embed=embed)
         return
 
-    # kullanıcı sayacı yoksa başlat
-    if user_id not in user_counters:
-        user_counters[user_id] = 0
-
-    user_counters[user_id] += 1
+    user_counters[user_id] = user_counters.get(user_id, 0) + 1
 
     if user_counters[user_id] > 10:
         user_counters[user_id] = 1
 
-    sayac = user_counters[user_id]
-
     embed = discord.Embed(
-        title=f"⏳ {sayac}/10",
-        description=f"{ctx.author.mention} için ilerleme devam ediyor...",
+        title=f"⏳ {user_counters[user_id]}/10",
+        description=f"{ctx.author.mention} ilerliyorsun… sabır (maalesef)",
         color=discord.Color.blue()
     )
 
     await ctx.send(embed=embed)
 
-bot.run("MTUwMDIyOTI2MTc1ODQzNTM4OA.Gc4xyw.KCrfwISjdmxcwIUBGvYrx3COTdfdp4QAZd7mCU")
+bot.run(os.getenv("TOKEN"))
